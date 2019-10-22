d69e40dc442b7f675faf9e6fb3f37e5340472627
Natali kruk <natasha_505@yahoo.com>
2019-10-22 04:43:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map_to_negativize(source_array)
     new_array.push( source_array[i] * -1 )
     i += 1
   end
-  return new_array
+return new_array
 end
 
 
@@ -16,7 +16,7 @@ def map_to_no_change(source_array)
     new_array.push(source_array[i])
     i +=1
   end
-  return new_array
+return new_array
 end
 
 
@@ -31,4 +31,12 @@ return new_array
 end
 
 
-
+def map_to_square(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do
+    new_array.push(source_array[1] * source_array[i])
+    i += 1
+end 
+return new_array
+end 
