6b6131debb3af998310edadbf89d24edda561cf5
Natali kruk <natasha_505@yahoo.com>
2019-10-22 04:44:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,7 +35,7 @@ def map_to_square(source_array)
   new_array = []
   i = 0 
   while i < source_array.length do
-    new_array.push(source_array[1] * source_array[i])
+    new_array.push(source_array[i] * source_array[i])
     i += 1
 end 
 return new_array
