475645d6e6dadf05cf552b2daca3891146598e4d
Natali kruk <natasha_505@yahoo.com>
2019-10-22 04:48:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,12 @@ return new_array
 end 
 
 
-def reduce_to_total(source_array, starting_point = 0)
-  new
-  
+def reduce_to_total(source_array, starting_point)
+  starting_point = 0 
+  i = 0 
+  while i < source_array.length do
+    starting_point += source_array[i]
+    i += 1 
+  end 
+return starting_point
 end 
