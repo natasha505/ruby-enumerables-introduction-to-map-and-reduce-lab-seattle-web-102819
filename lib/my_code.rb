04f3a26bdd8973363c9b629c76f19a5faba2999d
Natali kruk <natasha_505@yahoo.com>
2019-10-22 04:41:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,6 +8,7 @@ def map_to_negativize(source_array)
   return new_array
 end
 
+
 def map_to_no_change(source_array)
   new_array = []
   i = 0 
@@ -16,4 +17,15 @@ def map_to_no_change(source_array)
     i +=1
   end
   return new_array
+end
+
+
+def map_to_double(source_array)
+  new_array = []
+  i = 0 
+  while i < source_array.length do 
+    new_array.push(source_array[i] * 2)
+    i += 1 
+  end 
+return new_array
 end
